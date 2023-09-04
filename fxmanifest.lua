c8ee7c220f8b1bf9f8b971284929b651246928a0
math32d1 <94932668+math32d1@users.noreply.github.com>
2023-09-04 10:23:35 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+author 'Dalle far'
+description 'Ox_targets vrp'
+version '1.0.0'
+
+-- What to run
+client_scripts {
+  'client/main.lua',
+}
+
+server_scripts { 
+  'server/main.lua',
+}
+
+shared_scripts {
+  'config.lua'
+}
